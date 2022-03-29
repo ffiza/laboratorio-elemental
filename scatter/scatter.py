c501fad2a38222d4953ea9b28c558ae38442f617
Federico Iza <fgiza@protonmail.com>
2022-03-29 14:23:36 -0300

    Replace notebook with script.

---
+++ scatter/scatter.py
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------
+# Title:   Crear scatter plot
+# Author:  Federico Iza
+# ----------------------------------------------------------------------------
+import numpy as np
+import os
+import sys
+import matplotlib.pyplot as plt
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Agregar path para usar config.py
+from config import config_plots
+
+
+def main():
+    # Cargar datos
+    data = np.loadtxt('data.csv')
+    x, y = data[:, 0], data[:, 1]
+
+    # Generar gráfico
+    fig, ax = plt.subplots()
+    ax.minorticks_on()
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.set_xlim(0, 10)
+    ax.set_xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    ax.set_ylim(0, 20)
+    plt.scatter(x, y, s=15, linewidths=.5, edgecolors='white')
+    # plt.show()
+    fig.savefig('scatter.pdf')
+
+
+if __name__ == '__main__':
+    config_plots()
+    main()
